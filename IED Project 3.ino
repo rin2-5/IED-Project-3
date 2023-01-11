31cf8c6be63754e6331845ee0ce3d5109eff6d5a
ChuaTY <tze1227@gmail.com>
2023-01-11 10:05:24 +0800

    Added LCD (Scolling Message)

--- IED Project 3.ino
+++ IED Project 3.ino
@@ -1,3 +1,20 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+// addr,en,rw,rs,d4,d5,d6,d7,bl,blpol
+// addr can be 0x3F or 0x27
+
+const char message[] = "This is a scrolling message on an LCD"; // message to scroll
+
+
+int messageLen = sizeof(message) - 1; // length of message
+int pos = 0; // position of scrolling message
+unsigned long time = 0;
+unsigned long interval = 100;
+
+
 void setup() {
     //MOTORS
 #define Right1 9
@@ -13,6 +30,12 @@ void setup() {
 
     //FUNCTIONS
     void drive(int Rspeed, int Lspeed);
+
+    //LCD
+    lcd.init();            // initialize the lcd
+    lcd.backlight();       // turn on the backlight
+
+
 }
 
 void loop() {
@@ -60,7 +83,20 @@ void loop() {
     //Stop
     drive(0,0);
     delay(1000);
+    //LCD
+    if (millis() - time > interval) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(message + pos);
 
+        if (pos < messageLen - 16) {
+            pos++;
+        }
+        else {
+            pos = 0;
+        }
+        time = millis();
+    }
 
 }
 

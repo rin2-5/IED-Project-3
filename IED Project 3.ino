a547adfe5b8aa93f75935cb0704e8ad060b8de79
ChuaTY <tze1227@gmail.com>
2023-01-17 10:20:00 +0800

    finished LCD ver3 (on)

--- IED Project 3.ino
+++ IED Project 3.ino
@@ -1,11 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-// addr,en,rw,rs,d4,d5,d6,d7,bl,blpol
-// addr can be 0x3F or 0x27
-
 
 void setup() {
     //MOTORS
@@ -17,25 +13,63 @@ void setup() {
 #define Left2 5
     pinMode(Left1, OUTPUT);
     pinMode(Left2, OUTPUT);
-    //INFRARED
-    Serial.begin(9600);
-
-    //FUNCTIONS
-    void drive(int Rspeed, int Lspeed);
 
     //LCD
-  
     Serial.begin(9600);
     lcd.begin(16, 2);
     lcd.backlight();
     lcd.setCursor(0, 0);
-    lcd.print("Scrolling Texttttttttt");
+    lcd.print("Guide Dog-in-Training");
 
-
+    //FUNCTIONS
+    void drive(int Rspeed, int Lspeed);
 }
 
 void loop() {
+  //MOTORS 
+    //forward
+    drive(200, 200);
+    delay(1000);
+    drive(0, 0);
 
+    //reverse
+    drive(-255, -255);
+    delay(1000);
+    drive(0, 0);
+
+    //spin on the spot
+    drive(-255, 255); //anticlockwise
+    delay(1000);
+    drive(0, 0);
+    drive(255, -255); //clockwise
+    delay(1000);
+    drive(0, 0);
+
+    //forward right
+    drive(200, 130);
+    delay(1000);
+    drive(0, 0);
+
+    //forward left
+    drive(130, 200);
+    delay(1000);
+    drive(0, 0);
+
+    //reverse right
+    drive(-80, -180);
+    delay(1000);
+    drive(0, 0);
+
+    //reverse left
+    drive(-180, -80);
+    delay(1000);
+    drive(0, 0);
+
+    //Stop
+    drive(0, 0);
+    delay(1000);
+
+  //LCD 
     for (int i = 0; i < 15; i++) {
         lcd.scrollDisplayLeft();
         delay(200);
@@ -45,56 +79,12 @@ void loop() {
         delay(200);
     }
 
+  //TARGET BOARD 
 
-
-    int a;
-    Serial.println(analogRead(A1));
-
-    //forward
-    drive(200,200);
-    delay(1000);
-    drive(0,0);
-     
-    //reverse
-    drive(-255,-255);
-    delay(1000);
-    drive(0,0);
-  
-    //spin on the spot
-    drive(-255,255);//anticlockwise
-    delay(1000);
-    drive(0,0);
-    drive(255,-255);//clockwise
-    delay(1000);
-    drive(0,0);
-    
-    //forward right
-    drive(200,130);
-    delay(1000);
-    drive(0,0);
-    
-    //forward left
-    drive(130,200);
-    delay(1000);
-    drive(0,0);
-    
-    //reverse right
-    drive(-80,-180);
-    delay(1000);
-    drive(0,0);
- 
-    //reverse left
-    drive(-180,-80);
-    delay(1000);
-    drive(0,0);
-
-    //Stop
-    drive(0,0);
-    delay(1000);
-    
-
+  //ULTRASOUND & BUZZER 
 }
 
+//FUNCTIONS 
 void drive(int Rspeed, int Lspeed) {
     if (Rspeed >= 0)analogWrite(Right2, Rspeed);
     else analogWrite(Right1, abs(Rspeed));

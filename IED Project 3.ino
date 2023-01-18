d1a2a0bb200dba4f8892568a059f0ed201562171
ChuaTY <tze1227@gmail.com>
2023-01-18 08:32:19 +0800

    Buzzer,Ultrasound,LCD version 2

--- IED Project 3.ino
+++ IED Project 3.ino
@@ -2,6 +2,9 @@
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+int buzzer = 2;
+#define Tri_pin 11
+#define Echo_pin 10
 
 void setup() {
     //MOTORS
@@ -15,7 +18,7 @@ void setup() {
     pinMode(Left2, OUTPUT);
 
     //LCD
-    Serial.begin(9600);
+    //Serial.begin(9600);
     lcd.begin(16, 2);
     lcd.backlight();
     lcd.setCursor(0, 0);
@@ -24,9 +27,6 @@ void setup() {
     //TARGET BOARD 
 
     //ULTRASOUND & BUZZER 
-#define buzzer 9
-#define Tri_pin 11
-#define Echo_pin 10
     Serial.begin(9600);
     pinMode(Tri_pin, OUTPUT);
     pinMode(Echo_pin, INPUT);
@@ -37,62 +37,58 @@ void setup() {
 }
 
 void loop() {
-  //MOTORS 
-    //forward
-    drive(200, 200);
-    delay(1000);
-    drive(0, 0);
+    //MOTORS 
+      /*//forward
+      drive(200, 200);
+      delay(1000);
+      drive(0, 0);
 
-    //reverse
-    drive(-255, -255);
-    delay(1000);
-    drive(0, 0);
+      //reverse
+      drive(-255, -255);
+      delay(1000);
+      drive(0, 0);
 
-    //spin on the spot
-    drive(-255, 255); //anticlockwise
-    delay(1000);
-    drive(0, 0);
-    drive(255, -255); //clockwise
-    delay(1000);
-    drive(0, 0);
+      //spin on the spot
+      drive(-255, 255); //anticlockwise
+      delay(1000);
+      drive(0, 0);
+      drive(255, -255); //clockwise
+      delay(1000);
+      drive(0, 0);
 
-    //forward right
-    drive(200, 130);
-    delay(1000);
-    drive(0, 0);
+      //forward right
+      drive(200, 130);
+      delay(1000);
+      drive(0, 0);
 
-    //forward left
-    drive(130, 200);
-    delay(1000);
-    drive(0, 0);
+      //forward left
+      drive(130, 200);
+      delay(1000);
+      drive(0, 0);
 
-    //reverse right
-    drive(-80, -180);
-    delay(1000);
-    drive(0, 0);
+      //reverse right
+      drive(-80, -180);
+      delay(1000);
+      drive(0, 0);
 
-    //reverse left
-    drive(-180, -80);
-    delay(1000);
-    drive(0, 0);
+      //reverse left
+      drive(-180, -80);
+      delay(1000);
+      drive(0, 0);
 
-    //Stop
-    drive(0, 0);
-    delay(1000);
+      //Stop
+      drive(0, 0);
+      delay(1000);*/
 
-  //LCD 
+      //LCD 
     for (int i = 0; i < 15; i++) {
         lcd.scrollDisplayLeft();
-        delay(200);
-    }
-    for (int i = 0; i < 15; i++) {
-        lcd.scrollDisplayRight();
-        delay(200);
+        delay(100);
     }
 
-  //TARGET BOARD 
+    //TARGET BOARD 
 
-  //ULTRASOUND & BUZZER 
+    //ULTRASOUND & BUZZER 
     digitalWrite(Tri_pin, HIGH);
     delayMicroseconds(10);
     digitalWrite(Tri_pin, LOW);
@@ -100,7 +96,15 @@ void loop() {
     int dis = pulseIn(Echo_pin, HIGH) / 58;
     Serial.print(dis);
     Serial.println(" cm");
-    if (dis < 10) {
+    if (dis < 50 && dis>20) {
+        for (int i = 0; i < 100; i++) {
+            digitalWrite(buzzer, HIGH); // send high signal to buzzer 
+            delay(3); // delay 1ms
+            digitalWrite(buzzer, LOW); // send low signal to buzzer
+            delay(3);
+        }
+    }
+    else if (dis < 20) {
         for (int i = 0; i < 100; i++) {
             digitalWrite(buzzer, HIGH); // send high signal to buzzer 
             delay(1); // delay 1ms
